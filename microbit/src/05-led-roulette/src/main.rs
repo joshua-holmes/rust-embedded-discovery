32a6221e2819b5d21bd00827332f4ca2b0abfc60
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2023-06-15 17:29:22 -0700

    Build 5x5 letter generating system

--- microbit/src/05-led-roulette/src/main.rs
+++ microbit/src/05-led-roulette/src/main.rs
@@ -11,6 +11,13 @@ use microbit::{
     hal::{prelude::*, Timer},
 };
 
+mod vocab;
+
+
+fn print_to_leds(led_matrix: &mut [[u8; 5]; 5], msg: &str) {
+    
+}
+
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
@@ -18,16 +25,10 @@ fn main() -> ! {
     let mut timer = Timer::new(board.TIMER0);
     let mut display = Display::new(board.display_pins);
 
-    let light_it_all = [
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-    ];
+    let light_it_all = [[0; 5]; 5];
 
     loop {
-        display.show(&mut timer, light_it_all, 1000);
+        display.show(&mut timer, vocab::get_led_letter("Q"), 1000);
 
         display.clear();
         timer.delay_ms(1000u32);
