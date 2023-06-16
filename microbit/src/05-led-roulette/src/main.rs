3d8a7ee94d82a49ec5ec32dc2299cef8f50cdceb
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2023-06-16 00:07:20 -0700

    Create LED circles

--- microbit/src/05-led-roulette/src/main.rs
+++ microbit/src/05-led-roulette/src/main.rs
@@ -15,12 +15,57 @@ use microbit::{
 mod vocab;
 
 
-fn print_to_leds(timer: &mut Timer<TIMER0>, display: &mut Display, msg: &str, sec_per_letter: u32) {
+#[derive(Debug)]
+enum Direction {
+    GoRight,
+    GoDown,
+    GoLeft,
+    GoUp
+}
+
+impl Direction {
+    fn get_direction(x: usize, y: usize) -> Self {
+        if x < 4 && y == 0 {
+            Direction::GoRight
+        } else if x == 4 && y < 4 {
+            Direction::GoDown
+        } else if x > 0 && y == 4 {
+            Direction::GoLeft
+        } else if x == 0 && y > 0 {
+            Direction::GoUp
+        } else {
+            panic!("LEDs cannot move in a circle because of x and y: {}, {}", x, y);
+        }
+    }
+}
+
+
+fn print_to_leds(timer: &mut Timer<TIMER0>, display: &mut Display, msg: &str, speed: u8) {
     let mut scroller = vocab::Scroller::new(msg);
-    let delay_ms = sec_per_letter * 1000 / 5;
+    let delay_ms = 5500 / (speed as u32 + 10);
+    // let delay_ms = ms_per_letter / 5;
     
     while let Ok(screen) = scroller.scroll() {
         display.show(timer, screen, delay_ms);
+    }
+}
+
+fn led_circles(timer: &mut Timer<TIMER0>, display: &mut Display, speed: u8) {
+    let (mut x, mut y) = (0, 0);
+    let delay_ms = 5500 / (speed as u32 + 10);
+    
+    loop {
+        let mut screen = [[0; 5]; 5];
+        screen[y][x] = 1;
+        display.show(timer, screen, delay_ms);
+
+        let direction = Direction::get_direction(x, y);
+        match direction {
+            Direction::GoRight => x += 1,
+            Direction::GoDown  => y += 1,
+            Direction::GoLeft  => x -= 1,
+            Direction::GoUp    => y -= 1,
+        }
     }
 }
 
@@ -31,12 +76,8 @@ fn main() -> ! {
     let mut timer = Timer::new(board.TIMER0);
     let mut display = Display::new(board.display_pins);
 
-    print_to_leds(&mut timer, &mut display, "HELLO WORLD", 1);
+    print_to_leds(&mut timer, &mut display, "HELLO WORLD", 30);
 
-    loop {
-        display.show(&mut timer, vocab::get_led_letter(&'Q'), 1000);
-
-        display.clear();
-        timer.delay_ms(1000u32);
-    }
+    led_circles(&mut timer, &mut display, 100);
+    loop {}
 }
