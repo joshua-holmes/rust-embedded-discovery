ccf5d86988bf73d7a1e451e35e54265cdeef6e02
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2023-07-19 20:56:07 -0700

    Setup punch-o-meter

--- microbit/src/10-punch-o-meter/src/main.rs
+++ microbit/src/10-punch-o-meter/src/main.rs
@@ -3,6 +3,8 @@
 #![no_std]
 
 use cortex_m_rt::entry;
+use lsm303agr::Lsm303agr;
+use microbit::hal::twim;
 use rtt_target::rtt_init_print;
 use panic_rtt_target as _;
 
@@ -10,6 +12,13 @@ use panic_rtt_target as _;
 fn main() -> ! {
     rtt_init_print!();
     let board = microbit::Board::take().unwrap();
+
+    let i2c = twim::Twim::new(board.TWIM0, board.i2c_internal.into(), twim::Frequency::K100);
+
+    let mut sensor = Lsm303agr::new_with_i2c(i2c);
+    sensor.init().unwrap();
+    sensor.set_accel_odr(lsm303agr::AccelOutputDataRate::Hz100);
+    sensor.set_accel_scale(lsm303agr::AccelScale::G8);
 
     loop {}
 }
