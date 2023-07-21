606f70d80b570179d78483b540f4bb21319773bc
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2023-07-20 19:23:41 -0700

    Fix and finish punch-o-meter

--- microbit/src/10-punch-o-meter/src/main.rs
+++ microbit/src/10-punch-o-meter/src/main.rs
@@ -4,12 +4,7 @@
 
 use cortex_m_rt::entry;
 use lsm303agr::{Lsm303agr, AccelOutputDataRate};
-use microbit::{
-    hal::twim,
-    display::nonblocking::{
-        Display, GreyscaleImage
-    }
-};
+use microbit::{hal::{twim, Timer}, display::blocking::Display};
 use rtt_target::{rtt_init_print, rprintln};
 use panic_rtt_target as _;
 
@@ -23,7 +18,8 @@ const ACCEL_THRESHOLD: f32 = 1500.;
 fn main() -> ! {
     rtt_init_print!();
     let board = microbit::Board::take().unwrap();
-    let mut display = Display::new(board.TIMER0, board.display_pins);
+    let mut display = Display::new(board.display_pins);
+    let mut timer = Timer::new(board.TIMER0);
 
     let i2c = twim::Twim::new(board.TWIM0, board.i2c_internal.into(), twim::Frequency::K100);
 
@@ -31,31 +27,16 @@ fn main() -> ! {
     sensor.init().unwrap();
     sensor.set_accel_odr(AccelOutputDataRate::Hz100).unwrap();
     sensor.set_accel_scale(lsm303agr::AccelScale::G8).unwrap();
-    rprintln!("first");
 
-    let recording_screen = [
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 7, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-    ];
-    critical_section::with(|| {
-        display.show(&GreyscaleImage::new(&recording_screen));
-    });
-    rprintln!("second");
     loop {
-    rprintln!("in loop");
         while !sensor.accel_status().unwrap().xyz_new_data {}
         let data = sensor.accel_data().unwrap();
         let mag = calc::get_magnitude(data.x, data.y, data.z);
 
         if mag >= ACCEL_THRESHOLD {
-            rprintln!("met threshold");
-            display.show(&GreyscaleImage::new(&recording_screen));
             let max_g = calc::record_max_accel_as_i32(&mut sensor, AccelOutputDataRate::Hz100).unwrap();
             let screen = leds::num_to_screen(max_g).unwrap();
-            display.show(&GreyscaleImage::new(&screen));
+            display.show(&mut timer, screen, 1000);
         }
     }
 }
